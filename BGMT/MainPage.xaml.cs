2cd6425c7b2b7005819f18678feda3f94fcd59a9
Dominic Renales <Renales.Dominic@gmail.com>
2024-02-09 20:37:48 -0800

    Tied XAML buttons to code page
    Button Functionality Test

--- BGMT/MainPage.xaml.cs
+++ BGMT/MainPage.xaml.cs
@@ -1,13 +1,25 @@
-﻿namespace BGMT
+﻿using System.Runtime.CompilerServices;
+
+namespace BGMT
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
         }
+
+        // File Subitem Clicked Functionality
+        void NavBarFile_New(object sender, EventArgs e) { target.Text = "New Pressed"; }
+        void NavBarFile_Open(object sender, EventArgs e) { target.Text = "Open Pressed"; }
+        void NavBarFile_Import(object sender, EventArgs e) { target.Text = "Import Pressed"; }
+        void NavBarFile_Export(object sender, EventArgs e) { target.Text = "Export Pressed"; }
+        void NavBarFile_Exit(object sender, EventArgs e) { target.Text = "Exit Pressed"; }
+
+        // Help Subitem Clicked Functionality
+        void NavBarHelp_Settings(object sender, EventArgs e) { target.Text = "Settings Pressed"; }
+        void NavBarHelp_Help(object sender, EventArgs e) { target.Text = "Help Pressed"; }
     }
 
 }
