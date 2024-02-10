09b2b200ea2901b2f23ad1313fd218f4aa961400
Steven Heath <zoran.prower@gmail.com>
2024-02-09 22:38:31 -0500

    Add project files.

---
+++ BGMT/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BGMT
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BGMT/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BGMT
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
