0b22d465d65223b001dbca8dbe7cd9fa69210a70
Dominic Renales <Renales.Dominic@gmail.com>
2024-02-09 22:21:02 -0800

    More Buttons for Adding Mod Files
    Buttons for adding Dialogue, Scripts, and Installation files added.

--- BGMT/MainPage.xaml.cs
+++ BGMT/MainPage.xaml.cs
@@ -12,10 +12,10 @@ namespace BGMT
 
         }
 
-        // File Subitem Clicked Functionality
-        private async void NavBarFile_New(object sender, EventArgs e) 
+        //File Subitem Clicked Functionality
+        private async void NavBarFile_New(object sender, EventArgs e)
         {
-            try 
+            try
             {
                 var folder = await FolderPicker.PickAsync(default);
                 this.projectFolderPath = folder.Folder.Path;
@@ -24,27 +24,57 @@ namespace BGMT
                 this.menuItemImport.IsEnabled = true;
                 this.menuItemExport.IsEnabled = true;
             }
-            catch(Exception ex) { Console.WriteLine(ex.ToString()); }
+            catch (Exception ex) { Console.WriteLine(ex.ToString()); }
         }
-        void NavBarFile_Open(object sender, EventArgs e) 
-        { 
+        private void NavBarFile_Open(object sender, EventArgs e)
+        {
             target.Text = "Open Pressed";
 
         }
-        void NavBarFile_Import(object sender, EventArgs e) { target.Text = "Import Pressed"; }
-        void NavBarFile_Export(object sender, EventArgs e) { target.Text = "Export Pressed"; }
-        async void NavBarFile_Exit(object sender, EventArgs e)
+        private void NavBarFile_Import(object sender, EventArgs e)
+        {
+            target.Text = "Import Pressed";
+        }
+        private void NavBarFile_Export(object sender, EventArgs e)
+        {
+            target.Text = "Export Pressed";
+        }
+        private async Task NavBarFile_ExitAsync(object sender, EventArgs e)
         {
             bool answer = await DisplayAlert("Alert", "Are you sure you want to exit?", "Exit", "Cancel");
-            if (answer) 
+            if (answer)
             {
                 Application.Current?.Quit();
             }
+            target.Text = "Import Pressed";
+            target.Text = "Exit Pressed";
         }
 
-        // Help Subitem Clicked Functionality
-        void NavBarHelp_Settings(object sender, EventArgs e) { target.Text = "Settings Pressed"; }
-        void NavBarHelp_Help(object sender, EventArgs e) { target.Text = "Help Pressed"; }
+        //Help Subitem Clicked Functionality
+        private void NavBarHelp_Settings(object sender, EventArgs e)
+        {
+            target.Text = "Settings Pressed";
+        }
+        private void NavBarHelp_Help(object sender, EventArgs e)
+        {
+            target.Text = "Help Pressed";
+        }
+
+        //Mod Element Buttons
+        private void ModElement_AddDialogue(object sender, EventArgs e)
+        {
+            target.Text = "Dialogue Pressed";
+        }
+
+        private void ModElement_AddScript(object sender, EventArgs e)
+        {
+            target.Text = "Script Pressed";
+        }
+
+        private void ModElement_AddInstallation(object sender, EventArgs e)
+        {
+            target.Text = "Installation Pressed";
+        }
     }
 
 }
