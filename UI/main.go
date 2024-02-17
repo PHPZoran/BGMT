f2e0b4e8b585d551a70c1e43e50e2fdc4f1bb00d
Dominic Renales <Renales.Dominic@gmail.com>
2024-02-16 18:10:13 -0800

    Menu Bar Functionality
    Added:
    - Menu bar navigation items added to application *Redundant code in new and open items
    
    Modified:
    - New and Open Project buttons will now enable "Export Project" item when successfully opening a project file
    
    Removed:

--- UI/main.go
+++ UI/main.go
@@ -27,6 +27,51 @@ func main() {
 
 	//-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=
 
+	//File -> Menu Options
+	menuItemExportProject := fyne.NewMenuItem("Export Project", nil)
+	menuItemNewProject := fyne.NewMenuItem("New Project", func() {
+		utils.PromptForProjectName(myWindow, func(newPath string) {
+			state.SelectedDirectoryPath = newPath // Update the global variable with the new path
+
+			homeView := views.MakeHomeView(state.SelectedDirectoryPath, myWindow)
+			myWindow.SetContent(homeView) // Update the window content with the new tree
+		})
+		menuItemExportProject.Disabled = false
+	})
+	menuItemOpenProject := fyne.NewMenuItem("Open Project", func() {
+		dialog.ShowFolderOpen(func(uri fyne.ListableURI, err error) {
+			if uri == nil || err != nil {
+				return
+			}
+			state.SelectedDirectoryPath = uri.Path() // Store the selected directory path globally
+
+			if _, err := os.Stat(state.SelectedDirectoryPath + "/BGM.ini"); err != nil {
+				dialog.ShowError(errors.New("the selected folder is not a BGM Project folder"), myWindow)
+				return
+			}
+
+			homeView := views.MakeHomeView(state.SelectedDirectoryPath, myWindow)
+			myWindow.SetContent(homeView)
+		}, myWindow)
+		menuItemExportProject.Disabled = false
+	})
+
+	menuItemExportProject.Disabled = true
+
+	//Help -> Menu Options
+	menuItemReport := fyne.NewMenuItem("Report Bug", nil)
+	menuItemSettings := fyne.NewMenuItem("Settings", nil)
+	menuItemHelp := fyne.NewMenuItem("Help", nil)
+
+	//Create menus: File, Help
+	menuFile := fyne.NewMenu("File", menuItemNewProject, menuItemOpenProject, menuItemExportProject)
+	menuHelp := fyne.NewMenu("Help", menuItemReport, menuItemSettings, menuItemHelp)
+
+	//Set onto application window
+	myWindow.SetMainMenu(fyne.NewMainMenu(menuFile, menuHelp))
+
+	//-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=
+
 	// Button to allow user to select a directory
 	openProjectBtn := widget.NewButton("Open Project", func() {
 		dialog.ShowFolderOpen(func(uri fyne.ListableURI, err error) {
@@ -43,6 +88,7 @@ func main() {
 			homeView := views.MakeHomeView(state.SelectedDirectoryPath, myWindow)
 			myWindow.SetContent(homeView)
 		}, myWindow)
+		menuItemExportProject.Disabled = false
 	})
 
 	//-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=
@@ -56,6 +102,7 @@ func main() {
 			homeView := views.MakeHomeView(state.SelectedDirectoryPath, myWindow)
 			myWindow.SetContent(homeView) // Update the window content with the new tree
 		})
+		menuItemExportProject.Disabled = false
 	})
 
 	//-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=
