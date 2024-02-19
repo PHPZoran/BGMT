419f565301769cabaf40826bbc7a1b29c10ea86b
Dominic Renales <Renales.Dominic@gmail.com>
2024-02-18 19:51:51 -0800

    Updates
    Added:
    
    Modified:
    - Some variables in WeiDuConversionHelper to more properly explain their role
    - WeiDuConversionHelper to have less code redundancy
    
    Removed:
    - Helper code for zip files

--- UI/utils/exporthelper.go
+++ UI/utils/exporthelper.go
@@ -1,12 +1,10 @@
 package utils
 
 import (
-	"archive/zip"
 	"fmt"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/widget"
-	"io"
 	"os"
 )
 
@@ -34,10 +32,11 @@ func WeiDuFileConversion(window fyne.Window) {
 			return
 		}
 
-		WeiDuConversionHelper("dialogue")
-		WeiDuConversionHelper("script")
-		WeiDuConversionHelper("installation")
-		WeiDuConversionHelper("translation")
+		//Roadmapped for large file BGM to WeiDu
+		//WeiDuConversionHelper("dialogue")
+		//WeiDuConversionHelper("script")
+		//WeiDuConversionHelper("installation")
+		//WeiDuConversionHelper("translation")
 
 		dialog.ShowInformation("WeiDu File Status", "WeiDu file conversions are complete and can be found at:\n\n"+
 			GetParentDirectory()+"/"+filename, window)
@@ -106,65 +105,6 @@ func CreateZipFolder(filename string) bool {
 	}
 
 	return true
-}
-
-//-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=
-
-// ZipFiles compresses one or more files into a zip archive.
-// Credit: https://golangcode.com/create-zip-files-in-go/
-func ZipFiles(filename string, files []string) error {
-	newZipFile, err := os.Create(filename)
-	if err != nil {
-		return err
-	}
-	defer newZipFile.Close()
-
-	zipWriter := zip.NewWriter(newZipFile)
-	defer zipWriter.Close()
-
-	for _, file := range files {
-		if err := addFileToZip(zipWriter, file); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-// addFileToZip adds a file to the zip archive
-func addFileToZip(zipWriter *zip.Writer, filename string) error {
-	fileToZip, err := os.Open(filename)
-	if err != nil {
-		return err
-	}
-	defer fileToZip.Close()
-
-	// Get the file information
-	info, err := fileToZip.Stat()
-	if err != nil {
-		return err
-	}
-
-	// Create a header for the file
-	header, err := zip.FileInfoHeader(info)
-	if err != nil {
-		return err
-	}
-
-	// Set the name of the file inside the zip file
-	header.Name = filename
-
-	// Add metadata to the file header if needed
-	header.Method = zip.Deflate
-
-	// Create a writer for the file in the zip archive
-	writer, err := zipWriter.CreateHeader(header)
-	if err != nil {
-		return err
-	}
-
-	// Write the file data to the zip archive
-	_, err = io.Copy(writer, fileToZip)
-	return err
 }
 
 //-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=
