8adc3c7757d16649525110525ef97dd21dfa1ffa
BTCxZelko <BTCxZelko@protonmail.com>
2024-02-28 22:18:29 -0800

    code cleanup

--- UI/utils/exporthelper.go
+++ UI/utils/exporthelper.go
@@ -12,18 +12,6 @@ import (
 	"strings"
 )
 
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
-
-func checkDialog(e error, window fyne.Window) {
-	dialog.ShowInformation("Error", e.Error(), window)
-}
-
-//-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=
-
 func WeiDuFileConversion(window fyne.Window) {
 	var filename string
 	entry := widget.NewEntry()
@@ -38,115 +26,11 @@ func WeiDuFileConversion(window fyne.Window) {
 		fmt.Println(zipPath)
 		exportZip(zipPath)
 
-		//checkDialog(zipSource(GetInstallationDirectory(), zipPath), window)
-		//checkDialog(zipSource(GetTranslationDirectory(), zipPath), window)
-		//checkDialog(zipSource(GetScriptDirectory(), zipPath), window)
-		//checkDialog(zipSource(GetDialogueDirectory(), zipPath), window)
-
-		//if CreateZipFolder(GetParentDirectory() + "/" + filename + ".zip") {
-		//	dialog.ShowInformation("Error", "Error Creating Zip Folder", window)
-		//	return
-		//}
-
-		//Roadmap for later use
-		//WeiDuConversionHelper("dialogue")
-		//WeiDuConversionHelper("script")
-		//WeiDuConversionHelper("installation")
-		//WeiDuConversionHelper("translation")
-
 		dialog.ShowInformation("WeiDu File Status", "WeiDu file conversions are complete and can be found at:\n\n"+
 			GetParentDirectory()+"/"+filename, window)
 
 	}, window)
 	nameDialog.Show()
-}
-
-//-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=
-
-func WeiDuConversionHelper(mod string) {
-	var files []string
-
-	//Chain to consolidate all the folders that must be exported
-	if mod == "dialogue" {
-		directory, err := os.Open(GetDialogueDirectory())
-		check(err)
-		file, err := directory.ReadDir(-1)
-		check(err)
-
-		for _, file := range file {
-			files = append(files, GetDialogueDirectory()+"/"+file.Name())
-		}
-	} else if mod == "script" {
-		directory, err := os.Open(GetScriptDirectory())
-		check(err)
-		file, err := directory.ReadDir(-1)
-		check(err)
-
-		for _, file := range file {
-			files = append(files, GetScriptDirectory()+"/"+file.Name())
-		}
-	} else if mod == "installation" {
-		directory, err := os.Open(GetInstallationDirectory())
-		check(err)
-		file, err := directory.ReadDir(-1)
-		check(err)
-
-		for _, file := range file {
-			files = append(files, GetInstallationDirectory()+"/"+file.Name())
-		}
-	} else {
-		directory, err := os.Open(GetTranslationDirectory())
-		check(err)
-		file, err := directory.ReadDir(-1)
-		check(err)
-
-		for _, file := range file {
-			files = append(files, GetTranslationDirectory()+"/"+file.Name())
-		}
-	}
-
-	fmt.Println(files)
-	//JSONtoWeiDuHelper(mod, files)
-}
-
-//-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=
-
-func CreateZipFolder(filename string) bool {
-	fmt.Println(filename)
-	if _, err := os.Stat(filename); err != nil {
-		zipFolder, err := os.Create(filename)
-		check(err)
-		defer zipFolder.Close()
-
-		//filesDialogue := ReadDirectory(GetDialogueDirectory())
-		//filesScript := ReadDirectory(GetScriptDirectory())
-		//filesInstallation := ReadDirectory(GetInstallationDirectory())
-		//filesTranslation := ReadDirectory(GetTranslationDirectory())
-		//
-		//zipWriter := zip.NewWriter(zipFolder)
-
-		return false
-	}
-
-	return true
-}
-
-//-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=
-
-func ReadDirectory(directory string) []string {
-	var files []string
-
-	dir, err := os.Open(directory)
-	check(err)
-
-	file, err := dir.ReadDir(-1)
-	check(err)
-
-	for _, file := range file {
-		files = append(files, directory+"/"+file.Name())
-	}
-
-	return files
 }
 
 //-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=
@@ -160,12 +44,20 @@ func exportZip(zipFolder string) {
 		fmt.Println("Error creating zip file:", err)
 		return
 	}
-	defer zipFile.Close()
+	defer func() {
+		if err := zipFile.Close(); err != nil {
+			fmt.Println("Error closing zip file:", err)
+		}
+	}()
 
 	directories := []string{GetDialogueDirectory(), GetScriptDirectory(), GetTranslationDirectory(), GetCreatureDirectory()}
 
 	zipWriter := zip.NewWriter(zipFile)
-	defer zipWriter.Close()
+	defer func() {
+		if err := zipFile.Close(); err != nil {
+			fmt.Println("Error closing zip file:", err)
+		}
+	}()
 
 	// Copy Weidu_Compiler.exe as setup-ProjectName.exe in the zip
 	compilerPath := filepath.Join(projectDir, "Weidu_Compiler.exe")
@@ -240,7 +132,11 @@ func addFileToZip(zipWriter *zip.Writer, filePath, zipPath string) error {
 	if err != nil {
 		return err
 	}
-	defer file.Close()
+	defer func() {
+		if err := file.Close(); err != nil {
+			fmt.Println("Error closing file:", err)
+		}
+	}()
 
 	_, err = io.Copy(fileWriter, file)
 	return err
